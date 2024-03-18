8381b3f69fa0e503b270b43da8e63bea20a08e67
deyner2002 <solano27deiner@gmail.com>
2024-03-17 22:12:24 -0500

    first commit

---
+++ AuthenticationApi/Datos/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationApi.Datos
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ AuthenticationApi/Program.cs
@@ -0,0 +1,38 @@
+using AuthenticationApi.Datos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(opciones =>
+opciones.UseSqlServer("name=DefaultConnection")
+);
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().
+    AddEntityFrameworkStores<ApplicationDBContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
