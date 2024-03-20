7f0df0d672f1a0757781d5e1a4568068da969ccc
deyner2002 <solano27deiner@gmail.com>
2024-03-20 00:33:24 -0500

    remove using unnecessary

--- AuthenticationApi/Controllers/AuthenticationController.cs
+++ AuthenticationApi/Controllers/AuthenticationController.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Data.SqlClient;
 using AuthenticationApi.DTOs;
